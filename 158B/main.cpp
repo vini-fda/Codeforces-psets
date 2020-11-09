c27d28e3474ce85c1e15a58cec3279e882a1ac60
Vinicius Freitas de Almeida <vinifreitas.d.a@gmail.com>
2020-11-09 00:53:53 -0300

    Still [not working] 158B solution

--- 158B/main.cpp
+++ 158B/main.cpp
@@ -1,61 +1,58 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include <map>
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+int main()
 {
     int n; //groups of schoolchildren
-    /* 
-        a[0] -> number of groups with 1 child
-        a[1] -> number of groups with 2 children
-        a[2] -> number of groups with 3 children
-        a[3] -> number of groups with 4 children
-    */
-    int groups[4] = {0, 0, 0, 0}; //hashtable for amount of groups with a given amount of children
     cin >> n;
+
+    //hashtable for amount of groups with a given amount of children
+    map<uint8_t, uint32_t> groups;
+    groups[1] = groups[2] = groups[3] = groups[4] = 0;
+
+
     int k;
     for(int i=0; i < n; i++) {
         cin >> k;
         //increment #groups with k children
-        groups[k-1]++;
+        groups[k]++;
     }
-    //groups with 1 child are divided by 4(integer division)
-    //groups with 4 children already fill a taxi
-    int min_taxis = groups[0] / 4 + groups[3];
-    groups[0] %= 4;
-    //groups with 2 children can fit evenly in a taxi
-    //but there may be a remainder
-    min_taxis += groups[1] / 2;
-    groups[1] %= 2;
 
-    //use the remaining groups with 1 child with the one with 2 children
-    if(groups[0] != 0 && groups[1] != 0) {
-        if (groups[0] <= 2) 
-            groups[0] = 0;
-        else
-            groups[0] = 1;
-        groups[1]--;
+    int min_taxis = 0;
+    //4 children already fill a taxi
+    min_taxis += groups[4];
+    //2 groups of 2 children can fit evenly in a taxi
+    min_taxis += groups[2] / 2;
+    groups[2] %= 2;
+    //Lonely children can fit in a taxi
+    min_taxis += groups[1] / 4;
+    groups[1] %= 4;
+    //---------------
+    //Groups with 3 children may go with a lonely child
+    //If there are no more lonely children, there will
+    //be a vacant seat in the taxi
+    for(uint32_t i = 0; i < groups[3]; i++) {
+        if(groups[1] > 0)
+            groups[1]--;
         min_taxis++;
     }
-    //if there is still a lonely child...
-    if(groups[0] == 1) {
-        //put them with the group with 3 children
-        if(groups[2] != 0) {
-            groups[0] = 0;
-            groups[2]--;
-        } //or put them (alone) in a taxi
-        //either way, increase the count of taxis
-        min_taxis++;
-    } else if(groups[0] > 1) {
+    //If there is a couple of children
+    if(groups[2] == 1) {
+        //If there are 0, 1 or 2 lonely children
+        //put them with the couple
+        if(groups[1] <= 2)
+            groups[1] = 0;
+        else //if there are 3, take 2 and the other goes alone :(
+            groups[1] = 1;
         min_taxis++;
     }
-    //the remaining groups with 3 children can't be 
-    //evenly distributed in the taxis
-    min_taxis += groups[2];
-        
-    
+    //Add the (maybe) remaining child(ren) to a single taxi
+    if(groups[1] > 0)
+        min_taxis++;
+
+
     cout << min_taxis << endl;
     return 0;
 }
